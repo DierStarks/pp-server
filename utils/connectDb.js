9608de130eb4323bff589b9ccece98745b3b7424
DierStarks <166376713+DierStarks@users.noreply.github.com>
2025-02-25 16:45:16 -0800

    Update connectDb.js

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql2");
 
 // connecting to mysql server
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "@Password1",
+//   database: "testing_users",
+// });
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "@Password1",
-  database: "testing_users",
+  host: "sql7.freesqldatabase.com",
+  user: "sql7764741",
+  password: "5gQPPFCqDe",
+  database: "sql7764741",
 });
 
 module.exports = connection;
